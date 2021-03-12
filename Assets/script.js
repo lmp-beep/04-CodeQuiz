916ede96da7ef62282ded1e89f515df2e7d52da1
Lisa Pessin <lisapessin@gmail.com>
2021-03-12 11:41:33 -0500

    rebuilt line by line to fix button load issue

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,50 @@
 
-
 // Questions and Answers
 var questions = [
 	{
 		question : "Which is not a primitive data type?",
-		answers : ["Number","String", "Element", "Undefined"],
-		correct: "Element"
+		answers : [
+            { text: "Number", correct: false },
+            { text: "String", correct: false },
+            { text: "Element", correct: true },
+            { text: "Undefined", correct: false }
+        ]
 	},
 	{
 		question : "What function is used to change the background color?",
-		answers : ['document.color.background = ""', 'document.backgroundColor= ""', 'document.body.style.backgroundColor = ""', 'me.changeColor = """'],
-		correct: 'document.body.style.backgroundColor = ""'
+		answers : [
+            { text: 'document.color.background = ""', correct: false },
+            { text: 'document.backgroundColor= ""', correct: false },
+            { text: 'document.body.style.backgroundColor = ""', correct: true },
+            { text: 'me.changeColor = """', correct: false }
+        ]
 	},
     {
         question : "What company developed the scripting language JavaScript?",
-		answers : ["Microsoft", "Apple", "Mozilla", "Netscape"],
-		correct: "Netscape"
+		answers : [
+            { text: "Microsoft", correct: false },
+            { text: "Apple", correct: false },
+            { text: "Mozilla", correct: false },
+            { text: "Netscape", correct: true }
+        ]
     },
     {
         question : "What method is used to join two or more strings?",
-		answers : ["Concat()", "forEach()", "length()", "push()"],
-		correct: "Concat()"
+		answers : [
+            { text: "Concat()", correct: true },
+            { text: "forEach()", correct: false },
+            { text: "length()", correct: false },
+            { text: "push()",correct: false }
+        ]
     },
     {
         question : "What term is used to represent a non-existent or invalid value?",
-		answers : ["var", "Undefined", "Null", "isNAN"],
-		correct: "Null"
+		answers : [
+            { text: "isNAN", correct: false },
+            { text: "Null", correct: true },
+            { text: "var", correct: false },
+            { text: "Undefined", correct: false }
+        ]
     }
 ];
 
@@ -34,37 +53,33 @@ var questions = [
 
 
 
-// set variables
-var score = 0
-var questionIndex = 0
-
-var startButton = document.getElementById("startBtn");
+var startButton = document.getElementById("start-btn")
 var startButton = document.getElementById("introContainer");
-var questionContainerElement = document.getElementById("questionContainer");
-var questionElement = document.getElementById("question");
-var answerButtonsElement = document.getElementById("answerButtons");
+const nextButton = document.getElementById("next-btn")
+const questionContainerElement = document.getElementById("question-container")
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
 var timerEl = document.getElementById("timer");
-var nextButton = document.getElementById("nextBtn")
+
+let shuffledQuestions,  currentQuestionsIndex
 
 
-
-
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startGame)
 nextButton.addEventListener("click", () => {
-	questionIndex++
-	nextQuestion()
+    currentQuestionsIndex++
+    setNextQuestion()
 })
 
 
-function startQuiz() {
+
+function startGame() {
     console.log("I just got clicked");
-	startButton.classList.add("hide");
-	questionIndex = 0;
-	questionContainerElement.classList.remove("hide");
-	questionsAttempted = 0;
-	totalCorrect = 0;
-	startTimer();
-	showQuestion();
+    startButton.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionsIndex = 0
+    questionContainerElement.classList.remove("hide");
+    setNextQuestion();
+    startTimer();
 }
 
 
@@ -84,76 +99,66 @@ function startTimer() {
 } 
 
 
-function showQuestion() {
-    var currentQuestion = questions[questionIndex];
-    var headingElement = document.getElementById("question-heading");
-    headingElement.textContent = currentQuestion.question;
-    answerButtonsElement.innerHTML = "";
-    // answerButtonsElement.innerText = questions[0].answers
-    currentQuestion.answers.forEach(function (answer, i){
-        var button = document.createElement("button")
-        // adding the propety class and the value to the element in our HTML
-        button.classList.add("class", "answer");
-        // adding our value to the parameter createdd in this function called answer
-        // we have 2 parameter (answer $ i) the i reps the index
-        button.classList.add("value", answer);
-        // concatenate the index number before the array value
-        // so we are concatenating the index and period before each answer
-        button.textContent = answer;
-        // This appends the child button to the div with variable answerButtonsElement
-        answerButtonsElement.appendChild(button);
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionsIndex])
+}
 
-		button.addEventListener("click", selectAnswer)
-		answerButtonsElement.appendChild(button)
-    });
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        } 
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild
+    (answerButtonsElement.firstChild)
+}
+
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionsIndex + 1) {
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "Restart"
+        startButton.classList.remove("hide")
+    }
     
 }
 
 
-
-function selectAnswer(e) {
-	console.log("i got clicked");
-	const selectedButton = e.target
-	const correct = selectedButton.dataset.correct
-	setStatusClass(document.body, correct)
-	Array.from(answerButtonsElement.children).forEach(button => {
-		setStatusClass(button, button.dataset.correct)
-	})
-	nextButton.classList.remove("hide")
-}
-
 function setStatusClass(element, correct) {
-	clearStatusClass(element)
-	if (correct) {
-		element.classList.add("correct")
-	} else {
-		element.classList.add("wrong")
-	}
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
 }
+
 
 function clearStatusClass(element) {
-	element.classList.remove("correct")
-	element.classList.remove("wrong")
-}
-
-
-
-
-
-function nextQuestion () {
-	resetState();
-	showQuestion();
-}
-
-
-
-
-function resetState () {
-	nextButton.classList.add("hide");
-	while (answerButtonsElement.firstChild) {
-		answerButtonsElement.removeChild
-		(answerButtonsElement.firstChild)
-	}
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
 }
 
 
@@ -162,7 +167,7 @@ function gameOver() {
 }
 
 
-function displayScore () {
+function displayScore() {
 
 }
 
@@ -173,107 +178,5 @@ function submitScore() {
 
 
 function viewScoreboard() {
-
+    
 }
-
-
-
-
-
-
-
-
-// If answer is correct, continue to next question
-// If answer is wrong, deduct timer
-// var timer = 30;
-// if (question[0].correctAnswer === "Element") {
-    // go to the next question function should run here
-// 	currentQuestionIndex++
-// } else {
-//     var result = timer - 10;
-// }
-// console.log(result);
-
-// if (question[1].correctAnswer === 'document.body.style.backgroundColor = ""') {
-    // go to the next question function should run here
-// } else {
-//     var result = timer - 10;
-// }
-// console.log(result);
-
-// if (question[2].correctAnswer === "Netscape") {
-    // go to the next question function should run here
-// } else {
-//     var result = timer - 10;
-// }
-// console.log(result);
-
-// if (question[3].correctAnswer === "Concat()") {
-    // go to the next question function should run here
-// } else {
-//     var result = timer - 10;
-// }
-// console.log(result);
-
-// if (question[4].correctAnswer === "Null") {
-    // go to the next question function should run here
-// } else {
-//     var result = timer - 10;
-// }
-// console.log(result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if 
-// correct answer chosen
-// button turns green
-// increase score
-// background turns green
-
-
-// else
-// wrong answer chosen
-// button turns red
-// decrease score
-// background turns red
-// timer decrease
-
-
-// times up final score
-// enter name
-
-// finished questions final score
-// enter name
-
-// list of high scores
-
-
-
-// if else 3.9
-// for loops 3.13
-// functions 3.15
-// query selectors 4.5
-// set attributes 4.5
-// timers 4.9
-// eventListeners 4.11
-
-
-
-
-
-
-
-
-
-
-
